5cbab58eb0cd0331c5373751795d5be21a5077cd
Brandon Ellington <branjam4@gmail.com>
2020-11-26 01:21:26 -0800

    Add channel specifications

---
+++ channel-specs.scm
@@ -0,0 +1,15 @@
+(list (channel
+        (name 'emacs-28)
+        (url "https://github.com/flatwhatson/guix-channel")
+        (commit
+          "70557164862fce279263a64a13e0102dd709deb5"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (commit
+          "8791ef7cf667b44150c0e7e40655c90d79598353")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
