86f6dfc0625e23e1d7f3bdb9a3fbf7be0b6530fe
Brandon Ellington <branjam4@gmail.com>
2022-01-11 14:49:48 -0800

    Add experimental manifest
    I made this as a programmatic way to try packages out before I commit to
    a more official ~.scm~ file.

---
+++ experimental-emacs-manifest.scm
@@ -0,0 +1,17 @@
+(specifications->manifest
+ '("emacs-general"
+   "emacs-hercules"
+   "emacs-svg-tag-mode"
+   "emacs-svg-lib"
+   "emacs-svg-icon"
+   "emacs-emojify"
+   "emacs-web-server"
+   "emacs-names"
+   "emacs-exwm"
+   "emacs-emacsql"
+   "emacs-parseedn"
+   "emacs-ytel-show"
+   "emacs-ytel"
+   "emacs-edbi"
+   "emacs-gitpatch"
+   "emacs-engine-mode"))
