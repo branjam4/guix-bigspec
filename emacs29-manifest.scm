55dc7d79b0cab66cb4c54e5f1da509b48f0faa17
Brandon Ellington <branjam@gmail.com>
2022-09-14 05:07:22 -0700

    Add manifest for emacs 29

---
+++ emacs29-manifest.scm
@@ -0,0 +1,18 @@
+;; This "manifest" file can be passed to 'guix package -m' to reproduce
+;; the content of your profile.  This is "symbolic": it only specifies
+;; package names.  To reproduce the exact same profile, you also need to
+;; capture the channels being used, as returned by "guix describe".
+;; See the "Replicating Guix" section in the manual.
+
+(specifications->manifest
+  (list "emacs-gdscript-mode"
+        "godot"
+        "emacs-imenu-list"
+        "emacs-anzu"
+        "emacs-detached"
+        "guile"
+        "emacs-geiser"
+        "emacs-god-mode"
+        "emacs-use-package"
+        "emacs-guix"
+        "emacs-next"))
